9b9f033a41ef39903fb954f6977db518b50b1b92
dhubleur <dhubleur@student.42.fr>
2022-07-04 15:59:31 +0200

    get only login in events user get

--- events.py
+++ events.py
@@ -4,7 +4,7 @@ def get_events_ids(token: str, name: str):
     return [event['id'] for event in get(token, "/v2/events?campus_id=1&filter[name]=" + name)]
 
 def get_event_users(token: str, event_id: str):
-    return get(token, "/v2/events/" + str(event_id) + "/events_users")
+    return [item['user']['login'] for item in get(token, "/v2/events/" + str(event_id) + "/events_users")]
 
 def get_event_id(token: str, name: str):
     return get_events_ids(token, name)[0]
